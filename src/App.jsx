b45d77d13ed67a9e5ff74d7d79b6b8b38b8c0bd5
Hossain-Sajjad <sunshinems58@gmail.com>
2024-04-25 04:03:49 +0600

    solve some issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Todos from "./components/Todos";
 
 function App() {
   return (
-    <container>
+    <>
       <div className='flex justify-center mt-5'>
-        <h1 className='text-3xl'>Learn about redux toolkit</h1>
+        <h1 className='text-3xl'>Todo with redux toolkit</h1>
       </div>
       <div className='flex flex-col justify-center items-center'>
         <AddTodo />
         <Todos />
       </div>
-    </container>
+    </>
   );
 }
 
